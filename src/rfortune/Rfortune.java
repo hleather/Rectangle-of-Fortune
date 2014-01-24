8a0b461a7dc47ee16f1d81467ea85057bcce1b97
Heather <Heather@192.168.1.132>
2014-01-23 23:01:40 -0600

    Gal to Lit programming assignment.

--- src/rfortune/Rfortune.java
+++ src/rfortune/Rfortune.java
@@ -11,12 +11,43 @@ package rfortune;
  * @author Heather
  */
 public class Rfortune {
+class GameBoard {
+    char wordsPhrases;
+    char playerTurn;
+    int equations;
+}
+class Players {
+    char name;
+    int score;
+}
+class Menu {
+    char welcomeScreen;
+    char mMain;
+    char mHelp;
+    char mGame;
+}
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   
+    class displayName {
+public static void main(String args[]) {
+
+       Players player1 = new Players();
+       Players player2 = new Players();
+       
+       //assign values to the fields in player1
+       player1.name = "Sally";
+       player1.score = 100;
+       
+       //assign values to the fields in player2
+       player2.name = "Bob";
+       player2.score = 50;
+       
+       System.out.print("Player 1, " + player1.name + " has a score of " 
+       + player1.score + " . ");
+       System.out.print("Player 2, " + player2.name + " has a score of " 
+               + player2.score + " . ");
+
     }
     
+}
 }
