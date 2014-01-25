3870360f426c4998a305c43b78fda63a5306d962
Heather <Heather@192.168.1.132>
2014-01-25 12:48:49 -0600

    Rough outline

--- src/rfortune/Board.java
+++ src/rfortune/Board.java
@@ -6,10 +6,45 @@
 
 package rfortune;
 
+// We will import the design or layout of the game and have the code here
+
 /**
  *
  * @author Heather
  */
 public class Board {
+    int wordCount; // This needs to be an equation or command that counts the 
+                   // amount of letters in the current word or phrase.
+    
+    String playerName1;
+    String playerName2;
+    int bank1;
+    int bank2;
+    
+    // Location[][] letterLocations; This code will determine the location of 
+    // the letter guessed.
+    
+    // This class will also include the buttons that will display on the screen:
+    // Guess puzzle button
+    // Spin button
+    // Vowel button
+    // Pause/Exit button
+    
+    int spinButton(){
+        int guessWorth; // This would select a random amount of money (using 
+                        // some equation or code) that would
+                        // be awarded to the player whose turn it is if they
+                        // guessed a letter that was in the word/phrase.
+        return guessWorth;
+    }
+    public Board() {
+    }
+    
+    
+    public void displaySize() {
+        System.out.println("\nThe word or phrase is " + this.wordCount 
+                + " long.");
+        System.out.println("\n$" + spinButton());
+    }
     
 }
