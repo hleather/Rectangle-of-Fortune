36547eb5d6598755e143d725cd9366a50e4bc639
Heather <Heather@192.168.1.132>
2014-01-25 17:23:21 -0600

    Finished to-date Letters

--- src/rfortune/Letters.java
+++ src/rfortune/Letters.java
@@ -6,10 +6,36 @@
 
 package rfortune;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Heather
  */
 public class Letters {
+    Score player;
+    String guess = "c";
+    String wGuesses;
+    
+    public Letters() {
+        
+    }
+    
+    public void getGuess() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your guess: ");
+        this.guess = input.next();
+    }
+    
+    public void displayLetter() { 
+        if (this.guess != null) {
+            System.out.println(this.player.guess + " is correct!");
+        }
+        else {
+            System.out.println("Sorry " + this.guess + " is incorrect.");
+        }
+    }
+    
+    
     
 }
