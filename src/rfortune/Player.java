68ce5e81a408ba7eda8a81b7467436509f22e6ba
Heather <Heather@192.168.1.132>
2014-01-25 18:50:08 -0600

    Finished Player

---
+++ src/rfortune/Player.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rfortune;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Heather
+ */
+public class Player {
+    String numPlayers;
+    String player1 = "Susan";
+    String player2 = "Bob";
+    String player3 = "Dan";
+    
+    
+    public Player() {
+    }
+    
+    public void getNumberPlayers() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of players (one, two, or three): ");
+        this.numPlayers = input.next();       
+    }
+    
+    public void getPlayerNames () {
+        switch (this.numPlayers) {
+            case "three":
+                {
+                    Scanner input = new Scanner(System.in);
+                    System.out.println("Enter name of Player 1: ");
+                    this.player1 = input.next();
+                    System.out.println("Enter name of Player 2: ");
+                    this.player2 = input.next();
+                    System.out.println("Enter name of Player 3: ");
+                    this.player3 = input.next();
+                    break;
+                }
+            case "two":
+                {
+                    Scanner input = new Scanner(System.in);
+                    System.out.println("Enter name of Player 1: ");
+                    this.player1 = input.next();
+                    System.out.println("Enter name of Player 2: ");
+                    this.player2 = input.next();
+                    break;
+                }
+            default:
+                {
+                    Scanner input = new Scanner(System.in);
+                    System.out.println("Enter name of Player 1: ");
+                    this.player1 = input.next();
+                    break;
+                }
+         }
+        
+        }     
+ 
+    }
