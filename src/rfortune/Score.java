9c801529241507693434618f25fa5167b8399b29
Heather <Heather@192.168.1.132>
2014-01-25 19:39:12 -0600

    Finished Score

--- src/rfortune/Score.java
+++ src/rfortune/Score.java
@@ -6,24 +6,49 @@
 
 package rfortune;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Heather
  */
 public class Score {
-    String name;
-    String playerType;
+    Player player1;
+    Player player2;
+    Player player3;
     long rGuesses = 0;
     long wGuesses = 0;
     long phrasesCompleted = 0;
     String guess;
-    int amountMoney = 0;
+    String answer1;
+    String answer2;
+    String answer3;
     
     public Score() {
     }
         
-    public void displayName() {
-            System.out.println("\t\tHello " + this.name + " you currently have " 
-                    + this.amountMoney + " money.");
+    public void getAnswer() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Would you like to see your scores? "
+                + "(yes or no): ");
+        this.answer1 = input.next();            
         }
+   
+   
+    public void displayScore() {
+        switch (this.answer1) {
+            case "yes":
+            {
+                System.out.println("You have guessed letters correctly " + 
+                        this.rGuesses + " times. \nYou have guessed letters " + 
+                        "incorrectly " + this.wGuesses + " times."
+                        + "\nYou have guessed the correct"
+                        + " phrase " + this.phrasesCompleted + " times.");
+            }
+            case "no":
+            {
+                System.out.println(" ");
+            }
+        }
+    }
     }
