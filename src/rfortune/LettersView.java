f76b1656c8667418d5d1b249e07e4d6ecb1afb42
Heather <Heather@Heather-PC.nse.rr.com>
2014-02-12 20:31:21 -0600

    Merge origin/master
    Conflicts:
    	src/rfortune/GameMenuControl.java
    	src/rfortune/GamePreferencesControl.java

--- src/rfortune/LettersView.java
+++ src/rfortune/LettersView.java
@@ -1,15 +1,15 @@
-package rfortune;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+package rfortune;
+
 /**
  *
  * @author Heather
  */
-public class View {
+public class LettersView {
     
 }
