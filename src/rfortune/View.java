977292ceb719089538425ca5d68422a107046544
Heather <Heather@Heather-PC.nse.rr.com>
2014-02-12 20:28:43 -0600

    Merge origin/master
    Conflicts:
    	src/rfortune/GameMenuControl.java
    	src/rfortune/GamePreferencesControl.java

---
+++ src/rfortune/View.java
@@ -0,0 +1,15 @@
+package rfortune;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Heather
+ */
+public class View {
+    
+}
