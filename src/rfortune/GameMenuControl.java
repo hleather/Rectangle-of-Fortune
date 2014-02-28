3c44718bb1d6a921c3d789dd0c7ca2d8ea7ec2be
Litanels <litanels@gmail.com>
2014-02-27 19:23:48 -0600

    PlayerTurnView class added.

--- src/rfortune/GameMenuControl.java
+++ src/rfortune/GameMenuControl.java
@@ -20,7 +20,9 @@ public class GameMenuControl {
        
     
    public void takeTurn() {
-        System.out.println("\n\ttakeTurn() called");
+       //Would this work to call the getInput function from the GameTurnView class?
+        GameTurnView gameTurnView = new GameTurnView();
+        gameTurnView.getInput();
         
         System.out.println("The next letter guess is worth " + Bank.spinWorth);
     }
