a9346d8860d356f2822d94b8bfeb3739dc0fa94b
Heather <Heather@Heather-PC.nse.rr.com>
2014-03-08 21:44:41 -0600

    Almost there!

--- src/rfortune/CheckGuess.java
+++ src/rfortune/CheckGuess.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package rfortune;
 
 import java.io.Serializable;
@@ -11,13 +10,14 @@ import java.util.Scanner;
 
 /**
  * THESE (FUNCTIONS) ONLY CHECK TO SEE IF THE CONSONANT OR VOWEL IS A REPEAT AND
- * RETURNS A BOOLEAN VALUE. 
- * THIS CLASS HAS A VARIABLE THAT STORES THE CURRENT GUESS FOR EASY REFERENCE
- * AND A BOOLEAN THAT CAN BE USED TO DETERMINE WHETHER OR NOT TO RUN 
- * SEARCHPHRASE().
+ * RETURNS A BOOLEAN VALUE. THIS CLASS HAS A VARIABLE THAT STORES THE CURRENT
+ * GUESS FOR EASY REFERENCE AND A BOOLEAN THAT CAN BE USED TO DETERMINE WHETHER
+ * OR NOT TO RUN SEARCHPHRASE().
+ *
  * @author Heather
  */
 public class CheckGuess implements Serializable {
+
     private static boolean checkGuess = false;
     private static char currentGuess;
     private static boolean checkLetterRepeat = true;
@@ -140,165 +140,167 @@ public class CheckGuess implements Serializable {
         letter = aLetter;
     }
 
-    public CheckGuess(){
+    public CheckGuess() {
     }
-    
-    public static char[] checkLetterGuess(){
+
+    public static char[] checkLetterGuess() {
         Scanner newLetterGuess = Rfortune.getInputFile();
-            
-            System.out.println("\n\t-----------------------------------------------");
-            System.out.println("\t You have chosen to guess a consonant!");
-            System.out.println("\t-----------------------------------------------");
-            
-            int letterIndex = 0;
-            boolean done = false;
-            while (letterIndex <= 26 && !done){
-                System.out.println("\tEnter your guess: ");
-                char letterGuess;
-                letterGuess = newLetterGuess.next().toUpperCase().charAt(0);
-                CheckGuess.setCurrentGuess(letterGuess);
-                
-                if (!isVowel(letter, letterGuess)){
-                    new RfortuneError().displayError("Sorry, you have to buy "
-                            + "a vowel. Guess a letter. ");
-                    continue;
-                }
-                
-                if (alreadyInLetterList(getListOfLetterGuesses(), letterGuess)){
-                    new RfortuneError().displayError("That letter has already "
-                            + "been guessed. Try again.");
-                    continue;
-                }
-                
-                CheckGuess.getListOfLetterGuesses()[letterIndex] = letterGuess;
-                letterIndex++;
-                
-                if (!isCheckLetterRepeat()) {
-                    setCurrentGuess(letterGuess);
-                    setCheckGuess(true);
-                    done = true;
-                }           
-            }            
-                            
-            char[] newLetterGuessList = new char[letterIndex];
+
+        System.out.println("\n\t-----------------------------------------------");
+        System.out.println("\t You have chosen to guess a consonant!");
+        System.out.println("\t-----------------------------------------------");
+
+        int letterIndex = 0;
+        boolean done = false;
+        while (letterIndex <= 26 && !done) {
+            System.out.println("\tEnter your guess: ");
+            char letterGuess;
+            letterGuess = newLetterGuess.next().toUpperCase().charAt(0);
+            CheckGuess.setCurrentGuess(letterGuess);
+
+            if (!isVowel(letter, letterGuess)) {
+                new RfortuneError().displayError("Sorry, you have to buy "
+                        + "a vowel. Guess a letter. ");
+                continue;
+            }
+
+            if (alreadyInLetterList(getListOfLetterGuesses(), letterGuess)) {
+                new RfortuneError().displayError("That letter has already "
+                        + "been guessed. Try again.");
+                continue;
+            }
+
+            CheckGuess.getListOfLetterGuesses()[letterIndex] = letterGuess;
+            letterIndex++;
+
+            if (!isCheckLetterRepeat()) {
+                setCurrentGuess(letterGuess);
+                setCheckGuess(true);
+                done = true;
+            }
+        }
+
+        char[] newLetterGuessList = new char[letterIndex];
         System.arraycopy(CheckGuess.getListOfLetterGuesses(), 0, newLetterGuessList, 0, letterIndex);
-            
-            newLetterGuessList = CheckGuess.sortLetterList(newLetterGuessList);
-                        
-            return newLetterGuessList;
+
+        newLetterGuessList = CheckGuess.sortLetterList(newLetterGuessList);
+
+        return newLetterGuessList;
     }
-    
-    public static char[] checkVowelGuess(){
-        Bank.updateBankVowelPurchase();           
-        
-        if(Bank.isHasEnough())
-        {
+
+    public static char[] checkVowelGuess() {
+        Bank.updateBankVowelPurchase();
+
+        if (!Bank.isHasEnough()) {
+            System.out.println("Sorry, you do not have enough money to buy a "
+                    + "vowel.");
+        } else {
             Scanner newVowelGuess = Rfortune.getInputFile();
-            
+
             System.out.println("\n\t-----------------------------------------------");
             System.out.println("\t You have chosen to buy a vowel!");
             System.out.println("\t-----------------------------------------------");
-            
+
             int vowelIndex = 0;
             boolean done = false;
-            while (vowelIndex < 10 && !done){
+            while (vowelIndex < 10 && !done) {
                 System.out.println("\tEnter your guess: ");
                 char vowelGuess;
                 vowelGuess = newVowelGuess.next().toUpperCase().charAt(0);
-                
-                if (!isVowel(vowel, vowelGuess)){
+
+                if (!isVowel(vowel, vowelGuess)) {
                     new RfortuneError().displayError("That is not a vowel. "
                             + "Please enter a vowel. ");
                     continue;
                 }
-                
-                if (alreadyInVowelList(getListOfVowelGuesses(), vowelGuess)){
+
+                if (alreadyInVowelList(getListOfVowelGuesses(), vowelGuess)) {
                     new RfortuneError().displayError("That vowel has already "
                             + "been guessed. Try again.");
                     continue;
                 }
-                
+
                 CheckGuess.getListOfVowelGuesses()[vowelIndex] = vowelGuess;
                 vowelIndex++;
-                
+
                 if (!isCheckVowelRepeat()) {
                     setCurrentGuess(vowelGuess);
                     setCheckGuess(true);
                     done = true;
+                }
             }
-            
+
             char[] newVowelGuessList = new char[vowelIndex];
             System.arraycopy(CheckGuess.getListOfVowelGuesses(), 0, newVowelGuessList, 0, vowelIndex);
-            
+
             newVowelGuessList = CheckGuess.sortVowelList(newVowelGuessList);
-                        
+
             return newVowelGuessList;
         }
         return null;
     }
-    
-    public static char[] sortLetterList(char[] letters){
+
+    public static char[] sortLetterList(char[] letters) {
         char tempLetter;
         boolean notDone = true;
-        while(notDone){
-            
+        while (notDone) {
+
             notDone = false;
-            for (int i = 0; i < letters.length - 1; i++){
-               if (letters[i] < letters[i + 1])
-               {
-                   tempLetter = letters[i];
-                   letters[i] = letters[i+1];
-                   letters[i+1] = tempLetter;;
-                   notDone = true;
-               }
+            for (int i = 0; i < letters.length - 1; i++) {
+                if (letters[i] < letters[i + 1]) {
+                    tempLetter = letters[i];
+                    letters[i] = letters[i + 1];
+                    letters[i + 1] = tempLetter;;
+                    notDone = true;
+                }
             }
         }
-        
+
         return letters;
     }
-    public static char[] sortVowelList(char[] vowels){
+
+    public static char[] sortVowelList(char[] vowels) {
         char tempVowel;
         boolean notDone = true;
-        while(notDone){
-            
+        while (notDone) {
+
             notDone = false;
-            for (int i = 0; i < vowels.length - 1; i++){
-               if (vowels[i] < vowels[i + 1])
-               {
-                   tempVowel = vowels[i];
-                   vowels[i] = vowels[i+1];
-                   vowels[i+1] = tempVowel;;
-                   notDone = true;
-               }
+            for (int i = 0; i < vowels.length - 1; i++) {
+                if (vowels[i] < vowels[i + 1]) {
+                    tempVowel = vowels[i];
+                    vowels[i] = vowels[i + 1];
+                    vowels[i + 1] = tempVowel;;
+                    notDone = true;
+                }
             }
         }
-        
+
         return vowels;
     }
-       
+
     private static boolean alreadyInLetterList(char[] list, char value) {
-        for (char valueInList : list){
-            if(value == valueInList){
+        for (char valueInList : list) {
+            if (value == valueInList) {
                 return true;
             }
         }
         setCheckLetterRepeat(false);
         return false;
     }
-    
+
     private static boolean alreadyInVowelList(char[] list, char value) {
-        for (char valueInList : list){
-            if(value == valueInList){
+        for (char valueInList : list) {
+            if (value == valueInList) {
                 return true;
             }
         }
         setCheckVowelRepeat(false);
         return false;
     }
-    
+
     private static boolean isVowel(char[] list, char value) {
-        for (char valueInList : list){
-            if(value == valueInList){
+        for (char valueInList : list) {
+            if (value == valueInList) {
                 return true;
             }
         }
